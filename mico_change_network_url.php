5bc414d0e45d6cd368a3d51660b33861cdbf8fae
Nina Cecilie <nhoejholdt@gmail.com>
2015-02-16 18:09:55 +0100

    Add plugin name etc

--- mico_change_network_url.php
+++ mico_change_network_url.php
@@ -1,4 +1,11 @@
 <?php
+/*
+Plugin Name: MICO change network URL
+Description: Changes the network admin url, which allows installing Wordpress in a separate directory while using Mutisite.
+Version: 1.0
+Author: Malthe Milthers
+Author URI: http://www.mico.dk
+*/
 
 add_filter( 'network_admin_url', 'mico_change_network_url', 999, 2);
 
