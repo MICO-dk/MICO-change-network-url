ca23c20c03d6321608007b5a01b9716ab45ed10b
Malthe Milthers <malthe@milthers.dk>
2015-02-16 18:42:47 +0100

    change filter to use network_site_url
    the network_admin_url gave problems, resolved by hooking in to earlier function

--- mico_change_network_url.php
+++ mico_change_network_url.php
@@ -7,10 +7,23 @@ Author: Malthe Milthers
 Author URI: http://www.mico.dk
 */
 
-add_filter( 'network_admin_url', 'mico_change_network_url', 999, 2);
+add_filter( 'network_site_url', 'mico_change_network_url', 9, 3);
 
-function mico_change_network_url($url, $scheme) {
-    return network_site_url('wp/wp-admin/network/', $scheme);
+function mico_change_network_url($url, $path, $scheme) {
+    
+	if ( ! is_multisite() )
+		return site_url($path, $scheme);
+
+	$current_site = get_current_site();
+
+	if ( 'relative' == $scheme )
+		$url = $current_site->path;
+	else
+		$url = set_url_scheme( 'http://' . $current_site->domain . '/wp' . $current_site->path, $scheme );
+
+	if ( $path && is_string( $path ) )
+		$url .= ltrim( $path, '/' );
+
+
+    return $url;
 }
-
-?>
